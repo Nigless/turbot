ae97d37b77bbba5b940fccc5ba9ea3e544202fbc
Nigless <nigless.gaming@gmail.com>
2022-02-18 12:16:15 +0300

    add options for config

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,46 @@ use apis::console::Console;
 use apis::utils::iapi::IApi;
 use commands::root::Root;
 mod config_builder;
+use config_builder::ConfigBuilder;
+use std::env;
 mod dashes;
+use dashes::Dashes;
+use merge::Merge;
+use serde_derive::Deserialize;
+
+mod merge_types {}
+
+#[derive(Merge, Deserialize)]
+struct Config {
+	name: Option<String>,
+	#[merge(skip)]
+	version: String,
+}
 
 fn main() {
+	let args: Vec<String> = env::args().collect();
+
+	let mut config_path = None;
+	let config = ConfigBuilder::new(
+		"conf.toml",
+		r#"name = "rust"
+version = "0.1.0""#,
+	);
+
+	for arg in &args[1..] {
+		for option in Dashes::from(arg) {
+			match option {
+				("generate-config", path) | ("g", path) => config.generate(path).unwrap(),
+				("config-path", path) => config_path = Some(path.unwrap()),
+				(option, _) => return println!("unknown option: {}", option),
+			};
+		}
+	}
+
+	let config = config
+		.open::<Config>(&mut String::new(), config_path)
+		.unwrap();
+
 	let root = Root::new();
 	let console = Console::new(root);
 	console.start();
